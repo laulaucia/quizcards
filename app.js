24ee7c722528823b765ce89fe4767ae283a2061e
Laulaucia <buitrago.laura@gmail.com>
2015-12-07 17:50:45 -0800

    commented out some stuff that was hindering parse

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ var app = angular.module('quizCards', ['ui.router',
                           'ngResource', 'mcwebb.parse-patch']);
 app.config(function(ngParseProvider){
   ngParseProvider.initialize("uSFbSM6EMj5Zb2dCyuwQaPxzKuKkAy4sifhq9hWh", "A92NpYrHsWzXfP9OPcSzEI70aQgk6dL4KzfwvUb1");
-  var TestObject = ngParse.Object.extend("TestObject");
-  var testObject = new TestObject();
-  testObject.save({foo: "bar"}).then(function(object) {
-  alert("yay! it worked");
-});
+//   var TestObject = ngParse.Object.extend("TestObject");
+//   var testObject = new TestObject();
+//   testObject.save({foo: "bar"}).then(function(object) {
+//   alert("yay! it worked");
+// });
 });
 
 
