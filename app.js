c691b985395b268cad4f1710ef6e77e470082e17
Laulaucia <buitrago.laura@gmail.com>
2015-12-07 16:50:57 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+/*
+ * ANGULAR APP.JS
+ */
+
+var app = angular.module('quizCards', ['ui.router',
+                          'ngResource']);
+app.run(function(){
+  Parse.initialize("uSFbSM6EMj5Zb2dCyuwQaPxzKuKkAy4sifhq9hWh", "A92NpYrHsWzXfP9OPcSzEI70aQgk6dL4KzfwvUb1");
+  var TestObject = Parse.Object.extend("TestObject");
+  var testObject = new TestObject();
+  testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});
+});
+
+
+
+
+  app.config(['$stateProvider', '$locationProvider','$urlRouterProvider', function($stateProvider, $locationProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home',{
+        url: '/',
+        template: "I'm home, does this render?"
+      })
+      .state('decks', {
+        url: "/decks",
+        templateUrl: 'views/templates/decks-index.html',
+        controller: 'DecksIndexCtrl'
+      })
+      // .state('card', {
+      //   url: "/decks/cards/:id",
+      //   templateUrl: 'templates/cards-show.html',
+      //   controller: 'cardsController',
+      // })
+      .state('cards', {
+        url: "/cards",
+        templateUrl: 'views/templates/cards-index.html',
+        controller: 'cardsController'
+      });
+
+    $urlRouterProvider.otherwise("/");
+
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
+  }]);
