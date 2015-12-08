7e4597b2e12cb26ef91223711dffe478a462b745
Laulaucia <buitrago.laura@gmail.com>
2015-12-08 07:40:03 -0800

    trying to get something to console log

--- cardController.js
+++ cardController.js
@@ -2,24 +2,23 @@ var app = angular.module('quizCards', ['ui.router', 'ngResource']);
   app.run(function(){
     Parse.initialize("uSFbSM6EMj5Zb2dCyuwQaPxzKuKkAy4sifhq9hWh", "A92NpYrHsWzXfP9OPcSzEI70aQgk6dL4KzfwvUb1");
   });
-  
+
   app.controller('MainCtrl', ['$rootScope', '$scope', '$location', function ($rootScope, $scope, $location) {
     // INITIALIZATION AND NAVBAR LOGIC
   }]);
   app.controller('cardsController', ['$scope','$stateParams', 'CardService',function cardsController($scope, $stateParams, CardService){
 
-
-  var Card = Parse.Object.extend("CardService");
-  var card = new Card();
-  $scope.newCard= {};
-  $scope.cards = [];
-  $scope.cards = Card.query();
-  $scope.findCard = function(){
-    console.log($stateParams);
-    $scope.card = Card.get($stateParams.id, function(data){
-      console.log(data);
-    });
-  };
+    var Card = Parse.Object.extend("CardService");
+    var card = new Card();
+    $scope.newCard= {};
+    $scope.cards = [];
+    $scope.cards = CardService.query();
+    $scope.findCard = function(){
+      console.log($stateParams);
+      $scope.card = Card.get($stateParams.id, function(data){
+        console.log(data);
+      });
+    };
   //   {id: '1', prompt: "2 X 2", answer: "4", deck: "math"},
   //   {id: '2', prompt: "3 X 3", answer: "9", deck: "math" },
   //   {id: '3', prompt: "4 X 4", answer: "16", deck: "math"},
