b882ddccb901adfc884a941117889aa6ff976c82
Laulaucia <buitrago.laura@gmail.com>
2015-12-08 16:01:10 -0800

    took out attribute from factoryr

--- services.js
+++ services.js
@@ -12,9 +12,9 @@ angular.module('quizCards')
 			factory.save = function(Card){
 				var Card = Parse.Object.extend("Card");
 				var newCard = new Card(); // instantiating card object instance
-				newCard.set('prompt', Card.attribute.prompt);
-				newCard.set('answer', Card.attribute.answer);
-				newCard.set('deck', Card.attribute.deck);
+				newCard.set('prompt', Card.prompt);
+				newCard.set('answer', Card.answer);
+				newCard.set('deck', Card.deck);
 				// newCard.set("createdBy", Parse.User.current());
 				console.log("Card is: ", Card);
 				return newCard.save();
